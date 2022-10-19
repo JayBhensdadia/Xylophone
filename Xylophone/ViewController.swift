b168abb8bc48e8cea75749ff96f8c2495a6878b0
Jay Bhensdadia <jayvpatel29@gmail.com>
2022-10-19 21:37:51 +0530

    finishing app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player : AVAudioPlayer!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        
+        playAudio(whoSentIt: sender.currentTitle!)
+        
+    }
+    
+    
+    func playAudio(whoSentIt: String){
+        
+        if let asset = NSDataAsset(name: whoSentIt){
+            player = try! AVAudioPlayer(data:asset.data, fileTypeHint:"wav")
+            
+            player.play()
+        }
+        
+        
+        
+//        let url = Bundle.main.url(forResource: whoSentIt, withExtension: "wav")
+//        //player = try! AVAudioPlayer(contentsOf: url!)
+//
+//
+//        player.play()
+    }
+    
+    
 }
 
